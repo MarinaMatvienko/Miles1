420196f890ba61a86fc82da7787cff8e42ee7c10
Mary <mmatvienko111@gmail.com>
2022-11-19 00:00:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int tickets = 15000;
+        int minInMile = 20;
+        int bonus = tickets / minInMile;
+        System.out.println("Thank you for choosing our airline. Your bonus:"+ bonus +" miles");
+    }
+}
